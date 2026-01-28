c9d80574a9e38629d95340de190bed1784e84d47
= <=>
2026-01-28 08:07:50 +0200

    Conference Room update

--- ConferenceRoom.cs
+++ ConferenceRoom.cs
@@ -1,43 +1,26 @@
- 
-
-public enum BookingStatus 
-{ 
-    Pending,
-     Confirmed, 
-     Cancelled,
-      Completed 
-} 
-  
-public enum RoomType
- { 
-    FocusRoom,
-    StandardMeet,
-    Boardroom,
-    LargeRoom
-} 
-     
-
 public class ConferenceRoom
 { 
-        public Guid Id
-         { 
-            get;
-             } 
-             public string Name
-              { get;
-               private set;
-                } public int Capacity 
-                { get; 
-                private set; }
-                 public RoomType Type 
-                 { get;
-                  init; 
-                }
-                 // Encapsulation: Field is private, access is controlled. 
-                private readonly List<Booking> _bookings = new(); 
+    public Guid Id
+    { 
+       get;
+    } 
+    public string Name
+    { 
+        get;  
+    } 
+    public int Capacity 
+    { 
+        get; 
+    }
+    public RoomType Type 
+    {
+         get;
+         init; 
+    } 
+    private readonly List<Booking> _bookings = new(); 
                 public IReadOnlyCollection<Booking> Bookings => _bookings.AsReadOnly(); 
                 public ConferenceRoom(string name, int capacity, RoomType type)
-                 {
+                {
                      if (string.IsNullOrWhiteSpace(name)) 
                      throw new ArgumentException("Name required."); 
                      if (capacity <= 0) throw new ArgumentException("Capacity must be positive."); 
