f7679c8c5b4e0839074c19423bcfaedc67bc27b8
wendymathonsi12 <wendymathonsi24@outlook.com>
2026-01-29 11:22:06 +0200

    End of Assignment 1.3

--- obj/Debug/net8.0/ConferenceBookingDomain.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConferenceBookingDomain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConferenceBookingDomain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+384d55b0de30decd79ea521aa017ddf2b3254431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+111a0a6552970b39459cba9f237cf50d7c915fd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConferenceBookingDomain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConferenceBookingDomain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
